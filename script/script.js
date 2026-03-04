5eb373cacea9be10e79fa1d3e7ee28a3d7a5db35
ApuKM <apukumar180@gmail.com>
2026-03-04 19:57:31 +0600

    Handle errors, empty lessons for better ux

--- script/script.js
+++ script/script.js
@@ -30,16 +30,31 @@ const displayWordsByLevel = (dataArray) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (dataArray.length === 0) {
+    wordContainer.innerHTML = `
+    <div class= "text-center font-bangla col-span-3 py-8 space-y-2">
+      <img class="mx-auto" src="./assets/alert-error.png" alt="">
+      <p class="text-lg text-gray-600">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="text-2xl text-gray-800">নেক্সট Lesson এ যান</h2>
+    </div>
+    `;
+    return;
+  }
+
   dataArray.forEach((data) => {
     const div = document.createElement("div");
     div.innerHTML = `
-         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-3">
-        <h2 class="text-3xl font-bold">${data.word}</h2>
-        <p class="text-lg font-medium opacity-90">Meaning /Pronounciation</p>
-        <p class="text-2xl font-semibold font-bangla opacity-80">"${data.meaning} / ${data.pronunciation}"</p>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+          <h2 class="text-3xl font-bold">${data.word ? data.word : "শব্দ পাওয়া যায়নি"}</h2>
+          <p class="text-lg font-medium opacity-90">Meaning /Pronounciation</p>
+          <p class="text-2xl font-semibold font-bangla opacity-90">"${data.meaning ? data.meaning : "অর্থ পাওয়া যায়নি"} / ${data.pronunciation ? data.pronunciation : "Pronunciation পাওয়া যায়নি"}"</p>
+        <div class="flex justify-between items-center mt-10">
+          <button class="btn bg-gray-100"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-gray-100"><i class="fa-solid fa-volume-xmark"></i></button>
+        </div>
       </div>
         `;
-        wordContainer.appendChild(div);
+    wordContainer.appendChild(div);
   });
 };
 
